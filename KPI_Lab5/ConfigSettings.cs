94d66a92d556e739300ac678936c0b73a20d7dc1
Vitalii Yarema <vyarema@windowslive.com>
2018-12-23 18:23:49 +0200

    Creation

---
+++ KPI_Lab5/ConfigSettings.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+
+namespace KPI_Lab5
+{
+    public static class ConfigSettings
+    {
+        public static Dictionary<string, string> ReadAllSettings()
+        {
+            Dictionary<string, string> appsettings = new Dictionary<string, string>();
+            try
+            {
+                var appSettings = ConfigurationManager.AppSettings;
+
+                if (appSettings.Count == 0)
+                {
+                    Console.WriteLine("AppSettings is empty.");
+                }
+                else
+                {
+                    foreach (var key in appSettings.AllKeys)
+                    {
+                        Console.WriteLine("Key: {0} Value: {1}", key, appSettings[key]);
+                        appsettings.Add(key, appSettings[key]);
+                    }
+                }
+                return appsettings;
+            }
+            catch (ConfigurationErrorsException)
+            {
+                Console.WriteLine("Error reading app settings");
+                appsettings = new Dictionary<string, string>();
+                return appsettings;
+            }
+        }
+
+        public static string ReadSetting(string key)
+        {
+            try
+            {
+                var appSettings = ConfigurationManager.AppSettings;
+                string result = appSettings[key] ?? "Not Found";
+                Console.WriteLine(result);
+                return result;
+            }
+            catch (ConfigurationErrorsException)
+            {
+                Console.WriteLine("Error reading app settings");
+                return null;
+            }
+        }
+
+        public static void AddUpdateAppSettings(string key, string value)
+        {
+            try
+            {
+                var configFile = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+                var settings = configFile.AppSettings.Settings;
+                if (settings[key] == null)
+                {
+                    settings.Add(key, value);
+                }
+                else
+                {
+                    settings[key].Value = value;
+                }
+                configFile.Save(ConfigurationSaveMode.Modified);
+                ConfigurationManager.RefreshSection(configFile.AppSettings.SectionInformation.Name);
+            }
+            catch (ConfigurationErrorsException)
+            {
+                Console.WriteLine("Error writing app settings");
+            }
+        }
+
+        public static void RemoveSetting(string key)
+        {
+            try
+            {
+                var configFile = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+                var settings = configFile.AppSettings.Settings;
+                if (settings[key] != null)
+                {
+                    settings.Remove(key);
+                    configFile.Save(ConfigurationSaveMode.Modified);
+                    ConfigurationManager.RefreshSection(configFile.AppSettings.SectionInformation.Name);
+                }
+                else
+                {
+                    Console.WriteLine("There is not such settings");
+                }
+            }
+            catch (ConfigurationErrorsException)
+            {
+                Console.WriteLine("Error writing app settings");
+            }
+        }
+    }
+}
--- KPI_Lab5/MainWindow.xaml.cs
+++ KPI_Lab5/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -17,12 +18,62 @@ namespace KPI_Lab5
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
-    /// </summary>
+    /// </summary>    
     public partial class MainWindow : Window
     {
+        public Dictionary<string, string> AllSettings
+        {
+            get { return (Dictionary<string, string>)GetValue(AllSettingsProperty); }
+            set { SetValue(AllSettingsProperty, value); }
+        }
+        public static readonly DependencyProperty AllSettingsProperty = DependencyProperty.Register("AllSettings", typeof(Dictionary<string, string>), typeof(MainWindow), new PropertyMetadata(new Dictionary<string, string>()));
+
+        public KeyValuePair<string, string> CurrentSetting
+        {
+            get { return (KeyValuePair<string, string>)GetValue(CurrentSettingProperty); }
+            set { SetValue(CurrentSettingProperty, value); }
+        }
+        public static readonly DependencyProperty CurrentSettingProperty = DependencyProperty.Register("CurrentSetting", typeof(KeyValuePair<string, string>), typeof(MainWindow), new PropertyMetadata(new KeyValuePair<string, string>()));
+
         public MainWindow()
         {
+            DataContext = this;
             InitializeComponent();
+        }
+
+        private void ButtonAll_Click(object sender, RoutedEventArgs e)
+        {
+            AllSettings = ConfigSettings.ReadAllSettings();
+        }
+
+        private void ButtonAddUpdate_Click(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(TextBoxKey.Text))
+            {
+                ConfigSettings.AddUpdateAppSettings(TextBoxKey.Text.Trim(), TextBoxValue.Text.Trim());
+                AllSettings = ConfigSettings.ReadAllSettings();
+            }
+        }
+
+        private void ButtonRemove_Click(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(CurrentSetting.Key))
+            {
+                ConfigSettings.RemoveSetting(CurrentSetting.Key);
+                AllSettings = ConfigSettings.ReadAllSettings();
+            }
+        }
+
+        private void DataGridAppSettings_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (DataGridAppSettings.SelectedItem is KeyValuePair<string, string> kvp)
+            {
+                CurrentSetting = new KeyValuePair<string, string>(kvp.Key, kvp.Value);
+            }
+            else
+            {
+                CurrentSetting = new KeyValuePair<string, string>();
+            }
         }
     }
 }
